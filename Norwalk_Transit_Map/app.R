817974150a23d8dd020f8f7bba67aa0eeaa118ad
dkori <dave.kori@gmail.com>
2019-10-10 11:38:32 -0400

    Fixed issue of disappearing points (due to common layer ids), added directions on click

--- Norwalk_Transit_Map/app.R
+++ Norwalk_Transit_Map/app.R
@@ -15,9 +15,11 @@ library(ggplot2)
 library(sf)
 library(scales)
 library(leaflet.extras)
+library(shinythemes)
 # load("Norwalk_Proto3/transit_difficulties.rData")
 # test<-transit_difficulties[[1]][[1]][[1]]
 #create list of fixed routes
+#setwd("Norwalk_Transit_Map")
 fixed_route_list<-list.files("fixed routes/")
 #make the names of fixed route list human readable
 names(fixed_route_list)<-gsub('_',' ',fixed_route_list)
@@ -36,7 +38,7 @@ names(parking_list)<-gsub('\\.shx','',names(parking_list))
 parking_list[["none"]]<-"none"
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  
+  theme=shinytheme("slate"),
   # Application title
   titlePanel("Norwalk Transit Difficulty"),
   
@@ -54,8 +56,9 @@ ui <- fluidPage(
       radioButtons("departure_time",
                    "Departure Time",
                    c("Saturday, 11am"="1570892400",
-                     "Wednesday, 8am"="1570622400"),
-                   selected="1570622400"),
+                     "Saturday, 4pm"="1570910400",
+                     "Wednesday, 8am"="1571227200"),
+                   selected="1571227200"),
       radioButtons("transit_metric",
                    "Measure of Public Transit Difficulty",
                    c("Trip time (min)"="Total Transit Time",
@@ -80,44 +83,114 @@ ui <- fluidPage(
     ),
     
     # Show the map
-    mainPanel(HTML('This tool is meant to help planners identify gaps in mobility and wayfinding in Norwalk. 
-                    </br>
-                    </br>
+    mainPanel(HTML('This tool is meant to help planners identify <b>gaps in mobility and wayfinding</b> in Norwalk. 
+                   </br>
+                   </br>
                    <li>Choose a destination and departure time on the sidebar</li>
                    <li>a'),
-              tags$img(src="hiclipart.com-id_hyxcr.png",width="40px",height="30px"),
+              tags$img(src="not_found.png",width="35px",height="35px"),
               HTML('means no public transit is available from that location to the chosen destination*</li>
                    <li>colored dots show the difficulty of using public transit to reach the chosen destination</li>
+                   <li>
                    '),
+              tags$img(src="star_dark.png",width="35px",height="35px"),
+              HTML('indicates the selected destination</li>'),
               leafletOutput("map"),
-              "Click on a dot to view route information. Click between dots to reveal demographic information for the area. 
-              Estimates obtained using the Google Maps Directions API for October 9th, 2019 and October 12th, 2019.",
-              uiOutput(outputId="wheels_routes")
+              HTML("Click on a dot to view route information. Click between dots to reveal demographic information for the area.</br>"),
+              uiOutput(outputId="directions"),
+              HTML("</br>Estimates obtained using the Google Maps Directions API for October 12th, 2019 and October 16th, 2019.")
+              )
     )
     )
-  )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  
-  #paste the length of input$map_marker_click
-  output$wheels_routes<-renderText({
-    if(!is.null(input$map_marker_click[[1]])){
-      paste0("Transit Used: ",input$map_marker_click[[1]])
+  #create a reactive for map marker click
+  marker_click<-reactive({
+    input$map_marker_click[[1]]
+  })
+  # #paste the length of input$map_marker_click
+  # output$directions<-renderText({
+  #   # routes<-strsplit(marker_click(),", ", fixed=TRUE)
+  #   # route<-routes[[1]]
+  #   # route2<-gsub("Rte","Route",route)
+  #   # file_name<-paste0(gsub(" ","_",route2),
+  #   #                   ".kml")
+  #   # # for(route in routes){
+  #   # #   route2<-gsub("Rte","Route",route)
+  #   # #   file_name<-paste0(gsub(" ","_",route2),
+  #   # #                     ".kml")
+  #   # #   
+  #   # # }
+  #   # paste0(file_name%in%fixed_route_file_list)
+  #   if(!is.null(marker_click())){
+  #     marker_click
+  #   }
+  # })
+
+  #create an observer that loads wheels routes upon click
+  observe({
+    file_name<-"Wheels_Route_4.kml"
+    kml_list<-c()
+    #pull a list of all fixed routes
+    fixed_route_file_list<-list.files("fixed routes/")
+    if(!is.null(marker_click())){
+      routes<-strsplit(marker_click(),", ", fixed=TRUE)
+      for(i in length(routes)){
+        route<-routes[[1]][[i]]
+        route2<-gsub("Rte","Route",route)
+        file_name<-paste0(gsub(" ","_",route2),
+                          ".kml")
+        if(file_name%in%fixed_route_file_list){
+
+          #store routes in kml_list
+          kml_list<-c(kml_list,readr::read_file(paste0("fixed routes/",file_name)))
+
+        }
+        # given_route<-readr::read_file(paste0("fixed routes/",file_name))
+        if(length(kml_list)==2){
+          leafletProxy({"map"})%>%
+            clearGroup("click_route")%>%
+            addKML(kml=kml_list,
+                   weight=5,
+                   smoothFactor=0,
+                   color="#000000",
+                   fillOpacity=0.0,group="click_route")%>%
+            clearGroup("click_route2")%>%
+            addKML(kml=kml_list[[2]],
+                   weight=5,
+                   smoothFactor=0,
+                   color="#000000",
+                   fillOpacity=0.0,group="click_route2")
+        }else{
+          leafletProxy({"map"})%>%
+            clearGroup("click_route")%>%
+            addKML(kml=kml_list,
+                   weight=5,
+                   smoothFactor=0,
+                   color="#000000",
+                   fillOpacity=0.0,group="click_route")
+          
+        }
+
+
+
+      }
+
     }
   })
   #turn off scientific notation
   options(scipen=999)
-
+  
   #load the norwalk demographic stat information
   load("norwalk_stats.rData")
   #load the transit difficulties information
-  load("transit_difficulties.rData")
+  load("transit_difficulties6.RData")
   
   #load wheels2u boundary from kml
   wheels2u_boundary<-readr::read_file("Wheels2U Boundary.kml")
   
-
+  
   #delete row with no input in norwalk stats
   norwalk_stats<-norwalk_stats%>%filter(!is.na(Median_hh_income))%>%
     #add a column for the data in the popup
@@ -129,7 +202,7 @@ server <- function(input, output) {
                         paste0("<b>Poverty Rate: </b>", percent(`Poverty Rate`)),
                         paste0("<b>Median Household Income: </b>", dollar(`Median_hh_income`))))
   #create two data frames with some template numbers
-
+  
   #create a reactive object for destination choice
   chosen_destination<-reactive({
     paste0(input$destination)
@@ -168,37 +241,49 @@ server <- function(input, output) {
   #create react objects for available and unavailable routes based on radio button selections
   available<-reactive({
     transit_difficulties[[chosen_destination()]][[chosen_departure_time()]][["available"]]%>%
-      #remove geometry column
-      select(-geometry)%>%
       #since walking distance is in meters, make it miles
       mutate(`Total Distance to/from transit stops`=`Total Distance to/from transit stops`/1609.34)%>%
       #add a column for the data in the popup
       mutate(pop_up=paste(sep="<br/>",
+                          paste0("<b>Address:</b> ",gsub('\\+',' ',start_address)),
                           paste0("<b>Total Transit Time:</b> ",
                                  round(`Total Transit Time`,1)),
                           paste0("<b>Number of Transfers: </b>",`Number of Transfers`),
-                          paste0("<b>Total Distance to/from transit stops: </b>", 
+                          paste0("<b>Total Distance to/from transit stops: </b>",
                                  round(`Total Distance to/from transit stops`,1)),
                           paste0("<b>Transit Lines Used: </b>", `Transit Lines`)))%>%
       #limit only to stat selected
       dplyr::rename("chosen_stat"=chosen_stat())
   })
+  
+  #create a list of directions to display below map
+  output$directions<-reactive({
+    
+      if(!is.null(marker_click())){
+        temp<-available()%>%
+          dplyr::filter(start_address==marker_click())
+        paste0(temp$Instructions)
+      }
+      
+  })
   unavailable<-reactive({
+    #test<-transit_difficulties[["South+Norwalk+Train+Station,+Norwalk,+CT"]][[]]
     transit_difficulties[[chosen_destination()]][[chosen_departure_time()]][["unavailable"]]%>%
-      #remove geometry column
-      select(-geometry)%>%
-      select(-lat_long)%>%
-      as.data.frame()
+      as.data.frame()%>%
+      mutate(pop_up=paste(sep="<br/>",
+                          paste0("<b>Start Address:</b> ",start_address),
+                          paste0("<b>Soonest Available Departure:</b> ",soonest_departure),
+                          paste0("<b>Reason:</b> ",reason)))
   })
   
   #create color palette for chosen series
   pal <-reactive({
-      colorNumeric(palette = "RdYlGn", 
-                        domain = available()$chosen_stat, n = nrow(available()),
-                        reverse=TRUE)
-    })
-  
+    colorNumeric(palette = "RdYlGn",
+                 domain = available()$chosen_stat, n = nrow(available()),
+                 reverse=TRUE)
+  })
 
+  
   
   output$map<-renderLeaflet({
     
@@ -206,8 +291,8 @@ server <- function(input, output) {
     leaflet(
       width = "50%")%>%
       setView(-73.4167485, 41.101619, 12) %>%
-      addProviderTiles(provider = "Hydda.Full") 
-#        addProviderTiles(provider="CartoDB.Positron")
+      addProviderTiles(provider = "CartoDB.Positron") 
+    #        addProviderTiles(provider="CartoDB.Positron")
   })
   
   #create an observer that adds a layer if the wheels2u checkbox is selected
@@ -216,16 +301,16 @@ server <- function(input, output) {
       leafletProxy({"map"})%>%
         clearGroup("wheels2u")%>%
         addKML(kml=wheels2u_boundary,
-                    stroke=FALSE,
-                    opacity=.3,
-                    color="blue",
-                    group="wheels2u")
+               stroke=FALSE,
+               opacity=.3,
+               color="blue",
+               group="wheels2u")
     }else{
       leafletProxy({"map"})%>%
         clearGroup("wheels2u")
     }
   })
-  
+
   #create an observer that adds the selected fixed route line if one is selected
   observe({
     if(fixed_route()!="none"){
@@ -243,7 +328,8 @@ server <- function(input, output) {
         clearGroup("fixed")
     }
   })
-
+  # 
+  ?addPolygons
   #add the clear polygon layer to allow revealing demographic stats
   observe({
     leafletProxy("map")%>%
@@ -255,11 +341,18 @@ server <- function(input, output) {
                   #label group for this layer
                   group="poly")
   })
+  #create star icon
+  star<-makeIcon(
+    iconUrl="www/star_dark.png",
+    iconWidth = 35, iconHeight=35
+  )
+  
   #add the destination to the map
   observe({
     leafletProxy("map")%>%
       clearGroup("destination")%>%
       addMarkers(data=destination_layer(),
+                 icon=star,
                  group="destination")
   })
   #create parking icon
@@ -268,7 +361,7 @@ server <- function(input, output) {
     iconWidth = 15, iconHeight = 15#,
     # iconAnchorX = 22, iconAnchorY = 94,
   )
-  
+
   #add parking layer to map based on selection
   observe({
     if(parking()!="none"){
@@ -299,38 +392,43 @@ server <- function(input, output) {
   })
   #create redx icon
   redX <- makeIcon(
-    iconUrl = "www/hiclipart.com-id_hyxcr.png",
-    iconWidth = 20, iconHeight = 15#,
+    iconUrl = "www/not_found.png",
+    iconWidth = 20, iconHeight = 20#,
     # iconAnchorX = 22, iconAnchorY = 94,
   )
-  #add the layer for unavailable routes
-  observe({
-    leafletProxy("map")%>%
-      clearGroup("unavail")%>%
-      addMarkers(data=unavailable(),
-                 icon=redX,
-                 group="unavail")
-  })
-    #add the layer for available routes
+  
+
+  
+  # #add the layer for available routes
   observe({
     leafletProxy("map")%>%
       clearGroup("avail")%>%
-      addCircleMarkers(data=available(),popup =~pop_up,
-                       layerId = ~`Transit Lines`,
+      addCircleMarkers(data=available()%>%st_as_sf(),popup =~pop_up,
+                       layerId = ~`start_address`,
                        stroke=FALSE,
                        radius=10,
-                       fillOpacity=0.6,
-                        color=~pal()(chosen_stat),
+                       fillOpacity=0.8,
+                       color=~pal()(chosen_stat),
                        group="avail")
   })
-  #add the layer for unavailable routes
-  icons <- awesomeIconList(
-    times_circle = makeAwesomeIcon(icon = "times-circle", library = "fa", markerColor = "red")
-  )
+  # #add the layer for unavailable routes
+  observe({
+    leafletProxy("map")%>%
+      
+      clearGroup("unavail")%>%
+      addMarkers(data=unavailable()%>%st_as_sf(),popup=~pop_up,
+                 icon=redX,
+                 group="unavail")
+  })
+
+  # #add the layer for unavailable routes
+  # icons <- awesomeIconList(
+  #   times_circle = makeAwesomeIcon(icon = "times-circle", library = "fa", markerColor = "red")
+  # )
 
 
-  
-    # observe({
+
+  # observe({
   #   leafletProxy("map")%>%
   #     clearGroup("unavail")%>%
   #     addCircleMarkers(data=unavail,
@@ -345,12 +443,12 @@ server <- function(input, output) {
       addLegend(data=available(),"bottomright",
                 pal = pal(),
                 values = ~ chosen_stat,
-                title = "",
+                title = gsub('/','/</br>',chosen_stat()),
                 opacity = 1,
                 group="legend")
   })
-  
-  
+
+
 }
 
 # Run the application 
